152f73389a07e4b78233d6ce69f1822e45aca8f5
akamlani <akamlani@gmail.com>
2020-05-30 15:20:47 -0400

    add initial core utils

---
+++ hunter/core/nb_utils.py
@@ -0,0 +1,11 @@
+from    IPython import (display,)
+from    IPython.display import (Image, clear_output,)
+from    IPython.core.display import display, HTML
+from    IPython.core.debugger import set_trace          # use .set_trace() for debugging
+
+# %matplotlib inline
+# %config InlineBackend.figure_format = 'retina'
+import  matplotlib.pyplot as plt
+import  seaborn as sns 
+
+
